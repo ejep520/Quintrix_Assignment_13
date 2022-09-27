a1a3365989af45a24a046911082df8e9953b2cbf
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-09-27 09:55:34 -0700

    Add project files.

---
+++ Quintrix13th/README.md
@@ -0,0 +1,3 @@
+# Quintrix13th
+
+
---
+++ Quintrix13th/app.js
@@ -0,0 +1,19 @@
+'use strict';
+const fs = require("fs");
+const axios = require("axios");
+
+
+if (!fs.existsSync("./Hamlet3_1.txt")) {
+    console.log("The file Hamlet3_1.txt couldn't be found. Abend.");
+    exit(1);
+}
+
+fs.readFile("./Hamlet3_1.txt", "ascii", (e, data) => {
+    if (e) {
+        console.log(e.message);
+        return;
+    }
+    console.log(data);
+});
+
+axios.get("https://nodejs.org").then(resp => console.log(resp.data));
